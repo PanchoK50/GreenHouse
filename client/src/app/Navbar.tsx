1e15ed72ff0dcc8d0dce458c3856192dd64bb1fc
francisco <francisco.kusch@gmail.com>
2023-11-18 12:31:38 +0100

    pushing draft

--- client/src/app/Navbar.tsx
+++ client/src/app/Navbar.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
 
 function Navbar() {
+
+    const [activeButton, setActiveButton] = useState('button0');
+
+    const handleButtonClick = (buttonId: string) => {
+      console.log('Button clicked: ', buttonId)
+      setActiveButton(buttonId);
+      // You can perform additional actions when a button is clicked
+    };
+
   return (
     <>
       <div className="nav container">
-        <span className="link active">Dash</span>
-        <span className="link">Plan</span>
-        <span className="link">Rewards</span>
-        <span className="link">Facts</span>
+
+      <Link href="/" className={`link ${activeButton === 'button1' ? 'active' : ''}`} onClick={() => handleButtonClick('button1')}>
+        Button 1
+      </Link>
+
+      <Link href="/" className={`link ${activeButton === 'button2' ? 'active' : ''}`} onClick={() => handleButtonClick('button2')}>
+        Button 2
+      </Link>
+
+      <Link href="/" className={`link ${activeButton === 'button3' ? 'active' : ''}`} onClick={() => handleButtonClick('button3')}>
+        Button 3
+      </Link>
+
+      <Link href="/" className={`link ${activeButton === 'button4' ? 'active' : ''}`} onClick={() => handleButtonClick('button4')}>
+        Button 4
+      </Link>
+    
       </div>
     </>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
+import Chart from './Chart'
 
 export default function Home() {
   return (
     <>
+      <Chart />
       <h1>Hello World!</h1>
-      <FontAwesomeIcon icon={'earth-americas'} />
     </>
   )
 }
