cd75674029b98342f7274b2c6cd41697f2544638
francisco <francisco.kusch@gmail.com>
2023-11-19 01:54:08 +0100

    Add balance generation to Header component

--- client/src/app/Header.tsx
+++ client/src/app/Header.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEarthAmericas } from "@fortawesome/free-solid-svg-icons"
 
 function Header() {
+
+  // Function to generate a random number between 90 and 110
+  const generateRandomNumber = () : number => {
+    const min = 90;
+    const max = 110;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const randomBalance = generateRandomNumber();
+  // Generate the initial random number on component mount
+
   return (
     <>
         <header className="header">
@@ -11,7 +22,7 @@ function Header() {
               <span style={{color: "#007f21"}}>green</span>house.
             </h1>
             <p style={{fontSize: "1.5rem"}}>
-              100{" "}
+              {randomBalance}{" "}
               <span style={{color: "#007f21"}}>
                 <FontAwesomeIcon icon={faEarthAmericas} />
                 <i className="fa-solid fa-earth-americas"></i>
